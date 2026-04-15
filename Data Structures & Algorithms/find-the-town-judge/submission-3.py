c0353b364312b50095de0a81e8893073915c9cbb
pratyaksh1001 <76811387+pratyaksh1001@users.noreply.github.com>
2026-04-15 23:54:41 +0530

    Add: find-the-town-judge - submission-3

---
+++ Data Structures & Algorithms/find-the-town-judge/submission-3.py
@@ -0,0 +1,13 @@
+class Solution:
+    def findJudge(self, n: int, trust: List[List[int]]) -> int:
+        graph=[set() for _ in range(n)]
+        graph_2=[set() for _ in range(n)]
+        for link in trust:
+            a=link[0]
+            b=link[1]
+            graph[b-1].add(a)
+            graph_2[a-1].add(b)
+        for i in range(n):
+            if len(graph[i])==n-1 and len(graph_2[i])==0:
+                return i+1
+        return -1
